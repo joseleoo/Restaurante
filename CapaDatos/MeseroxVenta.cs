e0039a75c1cb2c09e01e5e3bafe4f99763f16bbe
Jose Leonardo Quiñones <56600373+joseleoo@users.noreply.github.com>
2020-06-10 11:19:18 -0500

    intento con raw

---
+++ CapaDatos/MeseroxVenta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+ public   class MeseroxVenta
+    {
+
+        public MeseroxVenta()
+        {
+            
+        }
+
+        public decimal IDDETALLEXFACTURA { get; set; }
+        public decimal NROFACTURA { get; set; }
+        public decimal VALOR { get; set; }
+        public decimal IDMESERO { get; set; }
+    }
+}
--- Restaurante/Indicadores.aspx.cs
+++ Restaurante/Indicadores.aspx.cs
@@ -24,54 +24,55 @@ namespace Restaurante
 
                 DateTime FechaFinal = DateTime.Parse(txtFechaFinal.Text);
                 Entities entities = new Entities();
-                
-                    var meseroList = (from m in entities.MESERO
-                                      join f in entities.FACTURA on m.IDMESERO equals f.IDMESERO into j
-                                      from  k in j.DefaultIfEmpty()
-                                      join d in entities.DETALLEXFACTURA on k.NROFACTURA equals d.NROFACTURA into detalles
-                                      from detalle in detalles.DefaultIfEmpty()
-                                      //where k.FECHA >= FechaInicial && k.FECHA <= FechaFinal
 
-                                      select new { idFactura = k ==null ? 0 : k.NROFACTURA, m.NOMBRES, m.APELLIDOS ,Ventas=detalle==null?0:detalle.VALOR}
-                                    
-                                      ).ToList();
+                //var meseroList = (from m in entities.MESERO
+                //                  join f in entities.FACTURA on m.IDMESERO equals f.IDMESERO into j
+                //                  from k in j.DefaultIfEmpty()
+                //                  join d in entities.DETALLEXFACTURA on k.NROFACTURA equals d.NROFACTURA into detalles
+                //                  from detalle in detalles.DefaultIfEmpty()
+                //                  orderby detalle.VALOR ascending
+                //                     //where k.FECHA >= FechaInicial && k.FECHA <= FechaFinal
 
-                //var group = (from mesero in meseroList
-                //             group mesero by mesero.NOMBRES into meserosGrupo
-                //             select new
-                //             {
-                          
-                //             }
+                //                      select new { idFactura = k ==null ? 0 : k.NROFACTURA, m.NOMBRES, m.APELLIDOS ,Ventas=detalle==null?0:detalle.VALOR}
 
-
-                    GridView1.DataSource = meseroList;
-                    GridView1.DataBind();
-
-                    //    ScriptManager.RegisterStartupScript(
-                    //             this, GetType(), "showalert", "alert('listo');", true);
-                    //}
-                    var consumo = decimal.Parse(txtCantidad.Text);
-                    
-                    var clientelist = (from m in entities.CLIENTE 
-                                       join f in entities.FACTURA on m.IDENTIFICACION equals f.IDCLIENTE
-                                
-                                       join d in entities.DETALLEXFACTURA on f.NROFACTURA equals d.NROFACTURA
-                                       where f.FECHA >= FechaInicial && f.FECHA <= FechaFinal && d.VALOR>= consumo
+                //                      ).ToList();
+                var meseroList = (entities.MESERO
+                   
+               .SqlQuery("select  m.idmesero,m.nombres, m.apellidos, m.edad , m. antiguedad,sum( d.valor) as valor " +
+                                 " from mesero m  left " +
+                                 " join factura f on f.idmesero = m.idmesero left " +
+                                 " join detallexfactura d on d.nrofactura = f.nrofactura " +
+                                " group by m.idmesero, m.nombres, m.apellidos, m.edad, m.antiguedad ")).ToList();
 
 
-                                       select new { m.IDENTIFICACION, d.NROFACTURA, m.NOMBRES, m.APELLIDOS , d.VALOR}
+                GridView1.DataSource = meseroList;
+                GridView1.DataBind();
 
-                                      ).ToList();
+                //    ScriptManager.RegisterStartupScript(
+                //             this, GetType(), "showalert", "alert('listo');", true);
+                //}
+                var consumo = decimal.Parse(txtCantidad.Text);
 
-                    var another = (from cliente in clientelist
-                                   group cliente by cliente.IDENTIFICACION);
+                var clientelist = (from m in entities.CLIENTE
+                                   join f in entities.FACTURA on m.IDENTIFICACION equals f.IDCLIENTE
 
-                    GridView2.DataSource = clientelist;
-                    GridView2.DataBind();
+                                   join d in entities.DETALLEXFACTURA on f.NROFACTURA equals d.NROFACTURA
+                                   where f.FECHA >= FechaInicial && f.FECHA <= FechaFinal && d.VALOR >= consumo
 
-                    ScriptManager.RegisterStartupScript(
-                             this, GetType(), "showalert", "alert('listo');", true);
-                
+
+                                   select new { m.IDENTIFICACION, d.NROFACTURA, m.NOMBRES, m.APELLIDOS, d.VALOR }
+
+                                  ).ToList();
+
+                var another = (from cliente in clientelist
+                               group cliente by cliente.IDENTIFICACION);
+
+                GridView2.DataSource = clientelist;
+                GridView2.DataBind();
+
+                ScriptManager.RegisterStartupScript(
+                         this, GetType(), "showalert", "alert('listo');", true);
+
             }
 
             catch (Exception ex)
